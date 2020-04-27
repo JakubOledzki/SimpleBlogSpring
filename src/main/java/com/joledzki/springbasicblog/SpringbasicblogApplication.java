86d15f7e5c6e25786f51c00333f46998683eb3cc
JakubOledzki <jakuboledzki21@gmail.com>
2020-04-27 20:46:42 +0200

    Create project

---
+++ src/main/java/com/joledzki/springbasicblog/SpringbasicblogApplication.java
@@ -0,0 +1,13 @@
+package com.joledzki.springbasicblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbasicblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joledzki/springbasicblog/SpringbasicblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joledzki.springbasicblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
